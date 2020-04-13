e95a775fc4b56a769df00abe486cb0b5c9a73375
林喜纯 <linxichun@chuangjiangx.com>
2020-04-13 14:09:03 +0800

    0.0.3内容提交； gitignore文件提交；

--- src/main/java/chou/may/gittest/tag/TagTest.java
+++ src/main/java/chou/may/gittest/tag/TagTest.java
@@ -6,6 +6,6 @@ package chou.may.gittest.tag;
  */
 public class TagTest {
     public static void main(String[] args) {
-        System.out.println("0.0.2");
+        System.out.println("0.0.3");
     }
 }
