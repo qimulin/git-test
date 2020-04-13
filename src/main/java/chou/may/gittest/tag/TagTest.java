54a4026917082a37952396a7c254ff30ce14b4e4
林喜纯 <linxichun@chuangjiangx.com>
2020-04-13 14:01:06 +0800

    0.0.2

---
+++ src/main/java/chou/may/gittest/tag/TagTest.java
@@ -0,0 +1,11 @@
+package chou.may.gittest.tag;
+
+/**
+ * @author Lin.xc
+ * @date 2020/4/13
+ */
+public class TagTest {
+    public static void main(String[] args) {
+        System.out.println("0.0.2");
+    }
+}
